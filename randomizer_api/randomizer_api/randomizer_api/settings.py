6efa556df7d106ac810783971a68444c3cf16b3e
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-19 14:13:55 -0600

    Remove django messages support

--- randomizer_api/randomizer_api/randomizer_api/settings.py
+++ randomizer_api/randomizer_api/randomizer_api/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
@@ -45,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
