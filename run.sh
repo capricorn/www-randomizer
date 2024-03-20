46b5fa4aabd2d33fa41cc480f7aa121840ce22ae
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-20 14:59:58 -0600

    Create script for running prod server

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROJ_ROOT="${HOME}/www-randomizer"
+
+cd randomizer_api
+DJANGO_DEBUG='False'\
+    SECRET_KEY_FILE='${PROJ_ROOT}/randomizer_api/randomizer_api/secret.txt'\
+    RANDOMIZER_URL_LIST="${PROJ_ROOT}/urls.json"\
+    poetry run python -m gunicorn randomizer_api.wsgi
