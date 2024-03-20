b330ec2faebbf4b5c66d9277ffb8fc84bd322be2
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-20 15:02:11 -0600

    Fix quotes for shell substitution

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ PROJ_ROOT="${HOME}/www-randomizer"
 
 cd randomizer_api/randomizer_api
 DJANGO_DEBUG='False'\
-    SECRET_KEY_FILE='${PROJ_ROOT}/randomizer_api/randomizer_api/secret.txt'\
+    SECRET_KEY_FILE="${PROJ_ROOT}/randomizer_api/randomizer_api/secret.txt"\
     RANDOMIZER_URL_LIST="${PROJ_ROOT}/urls.json"\
     poetry run python -m gunicorn randomizer_api.wsgi
