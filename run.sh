d522747c11f1e6209d6c31d3ec9ff426030c448c
capricorn <8250820+capricorn@users.noreply.github.com>
2024-03-20 15:01:31 -0600

    Fix django cd dir

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 PROJ_ROOT="${HOME}/www-randomizer"
 
-cd randomizer_api
+cd randomizer_api/randomizer_api
 DJANGO_DEBUG='False'\
     SECRET_KEY_FILE='${PROJ_ROOT}/randomizer_api/randomizer_api/secret.txt'\
     RANDOMIZER_URL_LIST="${PROJ_ROOT}/urls.json"\
